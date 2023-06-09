73023b3c100c43abe467d615570ad02d747e318f
Pratik Priyansh <pratikpriyansh5@gmail.com>
2023-06-09 16:09:57 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app=express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/',(req,res)=>{
+    res.render("index");
+})
+app.get("/login", function(req, res){
+    res.render("login");
+})
+app.get("/NGOlogin", function(req, res){
+    res.render("NGOlogin");
+})
+app.get("/signup", function(req, res){
+    res.render("signup");
+})
+app.get("/NGOsignup", function(req, res){
+    res.render("NGOsignup");
+})
+app.listen(3000,()=>{console.log("server running on port 3000")});
