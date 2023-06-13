b848efb4fe1c7aae1350002de08e16342a19bbd2
Umang Sinha <sinumang20@gmail.com>
2023-06-13 10:55:23 +0530

    commit message

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+
+const path=require('path');
 const app=express();
 const connectDB=require('./server/database/connection');
 app.use(express.static("public"));
@@ -24,4 +26,19 @@ app.get("/signup", function(req, res){
 app.get("/NGOsignup", function(req, res){
     res.render("NGOsignup");
 })
+/*
+app.post("/login",async(req,res)=>{
+    res.render("login");
+})
+app.post("/NGOlogin", async(req, res)=>{
+    res.render("NGOlogin");
+})
+app.post("/signup", async(req, res)=>{
+    res.render("signup");
+})
+app.post("/NGOsignup", async(req, res)=>{
+    res.render("NGOsignup");
+})*/
+app.use('/',require('./server/routes/router'));
+
 app.listen(3000,()=>{console.log("server running on port 3000")});
